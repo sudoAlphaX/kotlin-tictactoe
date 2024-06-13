bf756ce6f21c66fd0bb701684007df0816ac07d2
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2024-06-13 13:05:49 +0530

    use an algorithm to iteratively check the possible winning conditions and return

--- app/src/main/kotlin/org/game/App.kt
+++ app/src/main/kotlin/org/game/App.kt
@@ -19,9 +19,63 @@ class TicTacToeGame {
 """
         )
     }
+
+    fun checkWinner(lastMove: Char, canvas: List<List<Char>>): Char {
+        // Check row -
+        for (i in 0..2) {
+            var sj: Int = 0
+            for (j in 0..2) {
+                if (lastMove != canvas[i][j]) {
+                    break
+                }
+                sj ++
+            }
+            if (sj == 3) {
+               return lastMove
+            }
+        }
+        // Check Column |
+        for (j in 0..2) {
+            var si: Int = 0
+            for (i in 0..2) {
+                if (lastMove != canvas[i][j]) {
+                    break
+                }
+                si ++
+            }
+            if (si == 3) {
+                return lastMove
+            }
+        }
+
+        // Check Diagonal /
+        var sk: Int = 0
+        for (k in 0..2){
+            if (lastMove != canvas[k][k]) {
+                break
+            }
+        }
+        if (sk == 3) {
+            return lastMove
+        }
+
+        // Check Anti-Diagonal \
+        var sl: Int = 0
+        for (l in 0..2) {
+            val p: Int = l-2
+            if (lastMove != canvas[l][p]) {
+                break
+            }
+        }
+        if (sl == 3) {
+            return lastMove
+        }
+        return ' '
+    }
+
 }
 
 fun main() {
-    var match = TicTacToeGame()
+    val match = TicTacToeGame()
     match.printCanvas()
 }
